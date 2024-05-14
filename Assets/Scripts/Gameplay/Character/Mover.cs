6c3f5c23c36112ccd531b014896a2bfe0eba45f7
nnton3 <akarnomazov@gmail.com>
2024-05-14 19:08:00 +0300

    setup camera

--- Assets/Scripts/Gameplay/Character/Mover.cs
+++ Assets/Scripts/Gameplay/Character/Mover.cs
@@ -6,10 +6,13 @@ namespace Gameplay.Character
 {
 	public class Mover : MonoBehaviour, IDisposable
 	{
+		public bool IsMoving => _isMoving;
+
 		[SerializeField] private float _speed = 100f;
 		[SerializeField] private Rigidbody2D rb;
 		private IInput _input;
 		private Vector2 _direction;
+		private bool _isMoving;
 
 		[Inject]
 		public void Construct(IInput input)
@@ -18,11 +21,19 @@ namespace Gameplay.Character
 			_input.OnDirectionChanged += DirectionChangedHandler;
 		}
 
-		private void DirectionChangedHandler(Vector2 direction) => _direction = direction;
+		private void DirectionChangedHandler(Vector2 direction)
+		{
+			_direction = direction;
+			if (_direction == Vector2.zero && _isMoving)
+				_isMoving = false;
+			else if (_direction != Vector2.zero && !_isMoving)
+				_isMoving = true;
+		}
 
 		public void FixedUpdate()
 		{
-			rb.velocity = _direction * _speed * Time.deltaTime;
+			rb.MovePosition(rb.position + _direction * _speed * Time.fixedDeltaTime);
+			Debug.Log($"{rb}");
 		}
 
 		public void Dispose()
